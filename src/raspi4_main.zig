32c4549621260c93c4f3ef7004e96e82aabdd185
Carson <gamercarsonzero@gmail.com>
2024-12-10 22:07:06 -0500

    Blinky

--- src/raspi4_main.zig
+++ src/raspi4_main.zig
@@ -9,10 +9,23 @@ pub export fn main() void {
     var timer_module = TimerModule.init();
     system_data.write(SystemErds.erd.timer_module, &timer_module);
 
+    bcm2711.peripherals.GPIO.GPFSEL2 |= (0b001 << 21); // Set GPIO 27 as output
+
     while (true) {
         // const no_timers_expired = !timer_module.run();
         // if (no_timers_expired) {
         //     asm volatile ("sleep");
         // }
+        delay_cycles(5000000);
+        bcm2711.peripherals.GPIO.GPSET0 |= 1 << 27;
+        delay_cycles(5000000);
+        bcm2711.peripherals.GPIO.GPCLR0 |= 1 << 27;
+    }
+}
+
+fn delay_cycles(cycles: u32) void {
+    var count: u32 = 0;
+    while (count < cycles) : (count += 1) {
+        asm volatile ("nop");
     }
 }
