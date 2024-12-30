b597ed69a844c4c0e4bb8191dd9b9f49f5468b9c
Carson <gamercarsonzero@gmail.com>
2024-12-30 11:42:14 -0500

    Minimal reproduction works again

--- src/example_repro.zig
+++ src/example_repro.zig
@@ -2,22 +2,27 @@ const std = @import("std");
 
 const Erd = struct {
     T: type,
-    subs: comptime_int,
+    owner: ErdOwner,
+
+    pub const ErdOwner = enum {
+        Ram,
+        Indirect,
+    };
 };
 
 pub const ErdDefinitions = struct {
     // zig fmt: off
-    application_version:  Erd = .{ .T = u32,   .subs = 0 },
-    some_bool:            Erd = .{ .T = bool,  .subs = 3 },
-    unaligned_u16:        Erd = .{ .T = u16,   .subs = 1 },
-    always_42:            Erd = .{ .T = u16,   .subs = 0 },
-    pointer_to_something: Erd = .{ .T = ?*u16, .subs = 0 },
-    another_erd_plus_one: Erd = .{ .T = u16,   .subs = 0 },
+    application_version:  Erd = .{ .T = u32,   .owner = .Ram      },
+    some_bool:            Erd = .{ .T = bool,  .owner = .Ram      },
+    unaligned_u16:        Erd = .{ .T = u16,   .owner = .Ram      },
+    always_42:            Erd = .{ .T = u16,   .owner = .Indirect },
+    pointer_to_something: Erd = .{ .T = ?*u16, .owner = .Ram      },
+    another_erd_plus_one: Erd = .{ .T = u16,   .owner = .Indirect },
     // zig fmt: on
 };
 
 const erds = ErdDefinitions{};
 
 pub export fn main() void {
-    std.log.err("some_bool has {} subs", .{erds.some_bool.subs});
+    std.log.err("some_bool has type {}", .{erds.some_bool.T});
 }
