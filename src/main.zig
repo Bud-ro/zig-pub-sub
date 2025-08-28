565beb9c614492a36d8692a1748fc44a2e3f6103
unknown <github@carson.lol>
2025-08-28 00:13:46 -0400

    Stop being stupid

--- src/main.zig
+++ src/main.zig
@@ -6,10 +6,8 @@ const ErdJson = @import("erd_json.zig");
 pub fn main() !void {
     const max_json_size = comptime std.fmt.parseIntSizeSuffix("1MiB", 10) catch unreachable;
     var buf: [max_json_size]u8 = undefined;
-    var fba = std.heap.FixedBufferAllocator.init(&buf);
 
-    var out: std.io.Writer.Allocating = .init(fba.allocator());
-    defer out.deinit();
-    try ErdJson.generate_erd_json(&out.writer);
-    std.log.debug("System Erds as JSON Object:\n{s}", .{out.writer.buffered()});
+    var out = std.io.Writer.fixed(&buf);
+    try ErdJson.generate_erd_json(&out);
+    std.log.debug("System Erds as JSON Object:\n{s}", .{out.buffered()});
 }
